edab148c16698ad32384a15df3761cb9ae163800
chriscohoat <chris.cohoat@gmail.com>
2012-03-02 15:15:20 -0500

    Added setup.py and other necessary files.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-backup',
+    version     = '1.0.0',
+    author        = 'Chris Cohoat',
+    author_email = 'chris.cohoat@gmail.com',
+    url            = 'https://github.com/chriscohoat/django-backup',
+    description    = 'A backup script for the Django admin',
+    packages=find_packages(),
+    include_package_data=True,
+)
+
