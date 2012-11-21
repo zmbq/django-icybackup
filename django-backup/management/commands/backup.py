cf20c6bca2886c2d2b343c4c30881ca6a2769aa5
Adam Brenecki <abrenecki@sbtc.org.au>
2012-11-21 11:05:15 +1030

    remove email stuff

--- django-backup/management/commands/backup.py
+++ django-backup/management/commands/backup.py
@@ -1,5 +1,4 @@
-import os, popen2, time
-from datetime import datetime
+import os, time
 from optparse import make_option
 from tempfile import mkdtemp
 import tarfile
@@ -7,9 +6,7 @@ import tarfile
 from ... import db
 
 from django.core.management.base import BaseCommand, CommandError
-from django.core.mail import EmailMessage
 from django.conf import settings
-from django.contrib.sites.models import Site
 
 # Based on: http://code.google.com/p/django-backup/
 # Based on: http://www.djangosnippets.org/snippets/823/
@@ -20,15 +17,12 @@ class Command(BaseCommand):
             help='Write backup to file'),
         make_option('-d', '--outdir', default=None, dest='outdir',
         help='Write backup to timestamped file in a directory'),
-        make_option('--email', '-m', default=None, dest='email',
-            help='Sends email with attached dump file'),
         make_option('--extra', '-e', action='append', default=[], dest='extras',
             help='Include extra directories or files in the backup tarball'),
     )
     help = "Back up a Django installation (database and media directory)."
 
     def handle(self, *args, **options):
-        self.email = options.get('email')
         extras = options.get('extras')
         
         output_file = options.get('output')
@@ -71,23 +65,8 @@ class Command(BaseCommand):
             tf.add(media_root, arcname='backup/media')
             for extra in extras:
                 tf.add(extra, arcname='backup/extras/{}'.format(os.path.split(extra)[1]))
-
-        # Sending mail with backups
-        if self.email:
-            print "Sending e-mail with backups to '%s'" % self.email
-            self.sendmail(settings.SERVER_EMAIL, [self.email], dir_outfiles + [outfile])
         
         rm_rf(backup_root)
-
-    def sendmail(self, address_from, addresses_to, attachements):
-        subject = "Your DB-backup for %s %s" % (datetime.now().strftime("%d %b %Y"), self.current_site)
-        body = "Timestamp of the backup is " + datetime.now().strftime("%d %b %Y")
-
-        email = EmailMessage(subject, body, address_from, addresses_to)
-        email.content_subtype = 'html'
-        for attachement in attachements:
-            email.attach_file(attachement)
-        email.send()
         
 def rm_rf(d):
     for path in (os.path.join(d,f) for f in os.listdir(d)):
