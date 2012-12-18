fb6ee8fdfd97c748e40ad3746e64e7847e9dfdd4
Adam Brenecki <abrenecki@sbtc.org.au>
2012-12-18 13:40:16 +1030

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
+import subprocess
 
-setup(
-    name='django-backup',
-    version     = '1.0.1',
-    author        = 'Chris Cohoat',
-    author_email = 'chris.cohoat@gmail.com',
-    url            = 'https://github.com/chriscohoat/django-backup',
-    description    = 'A backup script for the Django admin',
+def get_long_desc():
+    """Use Pandoc to convert the readme to ReST for the PyPI."""
+    try:
+        return subprocess.check_output(['pandoc', '-f', 'markdown', '-t', 'rst', 'README.mdown'])
+    except:
+        print "WARNING: The long readme wasn't converted properly"
+
+setup(name='django-backup',
+    version='0.1dev',
+    description='A backup tool with local folder and Amazon Glacier support',
+    long_description=get_long_desc(),
+    author='Adam Brenecki',
+    author_email='adam@brenecki.id.au',
+    url='https://github.com/adambrenecki/django-icybackup',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        'boto',
+    ],
 )
-
