d8f220e5f4f3eed89647a6fc11805fcb997672b3
Adam Brenecki <abrenecki@sbtc.org.au>
2012-12-18 14:11:27 +1030

    Rename to icybackup

--- setup.py
+++ setup.py
@@ -8,15 +8,18 @@ def get_long_desc():
     except:
         print "WARNING: The long readme wasn't converted properly"
 
-setup(name='django-backup',
-    version='0.1dev',
-    description='A backup tool with local folder and Amazon Glacier support',
+setup(name='django-icybackup',
+    version='0.1pre',
+    description='A Django database/media backup tool with Amazon Glacier and local folder support',
     long_description=get_long_desc(),
-    author='Adam Brenecki',
-    author_email='adam@brenecki.id.au',
-    url='https://github.com/adambrenecki/django-icybackup',
+    author='Adam Brenecki, St Barnabas\' Theological College',
+    author_email='abrenecki@sbtc.org.au',
+    url='https://github.com/stbarnabas/django-icybackup',
     packages=find_packages(),
     include_package_data=True,
+    setup_requires=[
+        'setuptools_git>=0.3',
+    ],
     install_requires=[
         'boto',
     ],
