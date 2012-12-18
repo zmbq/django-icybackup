dfce9caaf8c8e877064556f71dda7c673028cb2a
Adam Brenecki <abrenecki@sbtc.org.au>
2012-12-18 12:05:13 +1030

    Upload to Amazon Glacier

--- django-backup/management/commands/backup.py
+++ django-backup/management/commands/backup.py
@@ -1,7 +1,9 @@
 import os, time
 from optparse import make_option
-from tempfile import mkdtemp
+from tempfile import mkdtemp, NamedTemporaryFile
 import tarfile
+from boto.glacier.layer2 import Layer2 as Glacier
+from django.conf import settings
 
 from ... import db
 
@@ -16,7 +18,9 @@ class Command(BaseCommand):
         make_option('-o', '--output', default=None, dest='output',
             help='Write backup to file'),
         make_option('-d', '--outdir', default=None, dest='outdir',
-        help='Write backup to timestamped file in a directory'),
+            help='Write backup to timestamped file in a directory'),
+        make_option('-g', '--glacier', default=None, dest='glacier',
+            help='Upload backup to the Amazon Glacier vault with the given ARN'),
         make_option('--extra', '-e', action='append', default=[], dest='extras',
             help='Include extra directories or files in the backup tarball'),
     )
@@ -27,6 +31,16 @@ class Command(BaseCommand):
         
         output_file = options.get('output')
         output_dir = options.get('outdir')
+        glacier_vault = options.get('glacier')
+        output_file_temporary = False
+        
+        # glacier backups go to a temporary file
+        if glacier_vault is not None:
+            output_file_temporary = True
+            output_file_obj = NamedTemporaryFile(delete=False)
+            output_file_obj.close() # we'll open it later
+            output_file = output_file_obj.name
+        
         if output_file is None:
             if output_dir is None:
                 raise CommandError('You must specify an output file')
@@ -66,6 +80,19 @@ class Command(BaseCommand):
             for extra in extras:
                 tf.add(extra, arcname='backup/extras/{}'.format(os.path.split(extra)[1]))
         
+        # upload to amazon glacier
+        if glacier_vault is not None:
+            g = Glacier(aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+            for i in g.list_vaults():
+                if glacier_vault == i.arn:
+                    vault = i
+                    break
+            else:
+                raise CommandError('The specified vault could not be accessed.')
+            vault.upload_archive(output_file)
+            os.unlink(output_file)
+        
+        # clean up
         rm_rf(backup_root)
         
 def rm_rf(d):
