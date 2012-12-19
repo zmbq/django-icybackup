711cb1a621853afc5a52e57be613924bb7344220
Adam Brenecki <abrenecki@sbtc.org.au>
2012-12-19 11:49:40 +1030

    Make backup records read-only

--- icybackup/admin.py
+++ icybackup/admin.py
@@ -4,5 +4,11 @@ from .models import GlacierBackup
 class GlacierBackupAdmin (admin.ModelAdmin):
 	list_display = ('date', 'glacier_id')
 	readonly_fields = ('glacier_id', 'date')
+	
+	def has_add_permission(self, request):
+		return False
+		
+	def has_delete_permission(self, request, obj=None):
+		return False
 
 admin.site.register(GlacierBackup, GlacierBackupAdmin)
