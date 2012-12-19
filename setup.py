314f9994378e111b2de9616bd6e2fdc3da8c7171
Adam Brenecki <abrenecki@sbtc.org.au>
2012-12-19 10:38:21 +1030

    Add version number to package, bump to 0.1a2

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 import subprocess
+from icybackup import __version__
 
 def get_long_desc():
     """Use Pandoc to convert the readme to ReST for the PyPI."""
@@ -9,7 +10,7 @@ def get_long_desc():
         print "WARNING: The long readme wasn't converted properly"
 
 setup(name='django-icybackup',
-    version='0.1pre1',
+    version=__version__,
     description='A Django database/media backup tool with Amazon Glacier and local folder support',
     long_description=get_long_desc(),
     author='Adam Brenecki, St Barnabas\' Theological College',
