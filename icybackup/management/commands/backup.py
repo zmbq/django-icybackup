941798c780a043eb4c56b38b7c6360c580c546d4
Itay Zandbank <zmbq@platonix.com>
2014-05-02 00:24:41 +0300

    Restore tabs in source files (they were automatically changed to spaces by PyCharm, by accident).

--- icybackup/management/commands/backup.py
+++ icybackup/management/commands/backup.py
@@ -11,85 +11,85 @@ from ...components import db, glacier
 # Based on: http://www.djangosnippets.org/snippets/823/
 # Based on: http://www.yashh.com/blog/2008/sep/05/django-database-backup-view/
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-    make_option('-o', '--output', default=None, dest='output',
-                help='Write backup to file'),
-    make_option('-d', '--outdir', default=None, dest='outdir',
-                help='Write backup to timestamped file in a directory'),
-    make_option('-g', '--glacier', default=None, dest='glacier',
-                help='Upload backup to the Amazon Glacier vault with the given ARN'),
-    make_option('-O', '--stdout', action='store_true', dest='stdout',
-                help='Output backup tarball to standard output'),
-    make_option('-n', '--no-media', action='store_false', dest='media', default=True,
-                help="Don't backup the media directory"),
-    make_option('--extra', '-e', action='append', default=[], dest='extras',
-                help='Include extra directories or files in the backup tarball'),
-    )
-    help = "Back up a Django installation (database and media directory)."
+	option_list = BaseCommand.option_list + (
+		make_option('-o', '--output', default=None, dest='output',
+		            help='Write backup to file'),
+		make_option('-d', '--outdir', default=None, dest='outdir',
+		            help='Write backup to timestamped file in a directory'),
+		make_option('-g', '--glacier', default=None, dest='glacier',
+		            help='Upload backup to the Amazon Glacier vault with the given ARN'),
+		make_option('-O', '--stdout', action='store_true', dest='stdout',
+		            help='Output backup tarball to standard output'),
+		make_option('-n', '--no-media', action='store_false', dest='media', default=True,
+		            help="Don't backup the media directory"),
+		make_option('--extra', '-e', action='append', default=[], dest='extras',
+		            help='Include extra directories or files in the backup tarball'),
+	)
+	help = "Back up a Django installation (database and media directory)."
 
-    def handle(self, *args, **options):
-        extras = options.get('extras')
+	def handle(self, *args, **options):
+		extras = options.get('extras')
 
-        output_file = options.get('output')
-        output_dir = options.get('outdir')
-        glacier_vault = options.get('glacier')
-        output_to_stdout = options.get('stdout')
-        backup_media = options.get('media')
-        output_file_temporary = False
+		output_file = options.get('output')
+		output_dir = options.get('outdir')
+		glacier_vault = options.get('glacier')
+		output_to_stdout = options.get('stdout')
+		backup_media = options.get('media')
+		output_file_temporary = False
 
-        # glacier backups go to a temporary file
-        if glacier_vault is not None or output_to_stdout:
-            output_file_temporary = True
-            output_file_obj = NamedTemporaryFile(delete=False)
-            output_file_obj.close()  # we'll open it later
-            output_file = output_file_obj.name
+		# glacier backups go to a temporary file
+		if glacier_vault is not None or output_to_stdout:
+			output_file_temporary = True
+			output_file_obj = NamedTemporaryFile(delete=False)
+			output_file_obj.close()  # we'll open it later
+			output_file = output_file_obj.name
 
-        if output_file is None:
-            if output_dir is None:
-                raise CommandError('You must specify an output file')
-            else:
-                output_file = os.path.join(output_dir, '{}.tgz'.format(_time()))
+		if output_file is None:
+			if output_dir is None:
+				raise CommandError('You must specify an output file')
+			else:
+				output_file = os.path.join(output_dir, '{}.tgz'.format(_time()))
 
-        media_root = settings.MEDIA_ROOT
+		media_root = settings.MEDIA_ROOT
 
-        # Create a temporary directory to perform our backup in
-        backup_root = mkdtemp()
-        database_root = os.path.join(backup_root, 'databases')
-        os.mkdir(database_root)
+		# Create a temporary directory to perform our backup in
+		backup_root = mkdtemp()
+		database_root = os.path.join(backup_root, 'databases')
+		os.mkdir(database_root)
 
-        # Back up databases
-        db.backup_to(settings, database_root)
+		# Back up databases
+		db.backup_to(settings, database_root)
 
-        # create backup gzipped tarball
-        with tarfile.open(output_file, 'w:gz') as tf:
-            tf.add(database_root, arcname='backup/databases')
-            if backup_media:
-                tf.add(media_root, arcname='backup/media')
-            if len(extras) > 0:
-                extras_mf = NamedTemporaryFile(delete=False)
-                for count, extra in enumerate(extras):
-                    tf.add(extra, arcname='backup/extras/{}'.format(count))
-                    extras_mf.write('{},{}\n'.format(count, extra.replace(',', '\\,')))
-                extras_mf.close()
-                tf.add(extras_mf.name, arcname='backup/extras/manifest')
-                os.unlink(extras_mf.name)
+		# create backup gzipped tarball
+		with tarfile.open(output_file, 'w:gz') as tf:
+			tf.add(database_root, arcname='backup/databases')
+			if backup_media:
+				tf.add(media_root, arcname='backup/media')
+			if len(extras) > 0:
+				extras_mf = NamedTemporaryFile(delete=False)
+				for count, extra in enumerate(extras):
+					tf.add(extra, arcname='backup/extras/{}'.format(count))
+					extras_mf.write('{},{}\n'.format(count, extra.replace(',', '\\,')))
+				extras_mf.close()
+				tf.add(extras_mf.name, arcname='backup/extras/manifest')
+				os.unlink(extras_mf.name)
 
-        # upload to glacier
-        if glacier_vault is not None:
-            glacier.upload(glacier_vault, output_file, settings)
-            glacier.reconcile(glacier_vault, settings)
-            glacier.prune(glacier_vault, settings)
+		# upload to glacier
+		if glacier_vault is not None:
+			glacier.upload(glacier_vault, output_file, settings)
+			glacier.reconcile(glacier_vault, settings)
+			glacier.prune(glacier_vault, settings)
 
-        # output to stdout
-        if output_to_stdout:
-            with open(output_file, 'r') as f:
-                sys.stdout.write(f.read())
+		# output to stdout
+		if output_to_stdout:
+			with open(output_file, 'r') as f:
+				sys.stdout.write(f.read())
 
-        # clean up
-        shutil.rmtree(backup_root, ignore_errors=True)
-        if output_file_temporary:
-            os.unlink(output_file)
+		# clean up
+		shutil.rmtree(backup_root, ignore_errors=True)
+		if output_file_temporary:
+			os.unlink(output_file)
 
 
 def _time():
-    return time.strftime('%Y%m%d-%H%M%S')
+	return time.strftime('%Y%m%d-%H%M%S')
--- icybackup/management/commands/restore.py
+++ icybackup/management/commands/restore.py
@@ -13,57 +13,57 @@ from ...components import db
 # Based on: http://www.djangosnippets.org/snippets/823/
 # Based on: http://www.yashh.com/blog/2008/sep/05/django-database-backup-view/
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-    make_option('-i', '--file', default=None, dest='input',
-                help='Read backup from file'),
-    make_option('--pg-restore-flags', default=None, dest='postgres_flags',
-                help='Flags to pass to pg_restore'),
-    make_option('-I', '--stdin', action='store_true', dest='stdin',
-                help='Read backup from standard input'),
-    )
-    help = "Restore a Django installation (database and media directory)."
+	option_list = BaseCommand.option_list + (
+		make_option('-i', '--file', default=None, dest='input',
+		            help='Read backup from file'),
+		make_option('--pg-restore-flags', default=None, dest='postgres_flags',
+		            help='Flags to pass to pg_restore'),
+		make_option('-I', '--stdin', action='store_true', dest='stdin',
+		            help='Read backup from standard input'),
+	)
+	help = "Restore a Django installation (database and media directory)."
 
-    def handle(self, *args, **options):
-        extras = options.get('extras')
+	def handle(self, *args, **options):
+		extras = options.get('extras')
 
-        input_file = options.get('input')
-        input_from_stdin = options.get('stdin')
-        input_file_temporary = False
+		input_file = options.get('input')
+		input_from_stdin = options.get('stdin')
+		input_file_temporary = False
 
-        if input_file is None and input_from_stdin is None:
-            raise CommandError('You must specify an input file')
+		if input_file is None and input_from_stdin is None:
+			raise CommandError('You must specify an input file')
 
-        media_root = settings.MEDIA_ROOT
+		media_root = settings.MEDIA_ROOT
 
-        # read from stdin
-        if input_from_stdin:
-            input_file_temporary = True
-            input_file_obj = NamedTemporaryFile(delete=False)
-            input_file_obj.write(sys.stdin.read())
-            input_file_obj.close()
-            input_file = input_file_obj.name
+		# read from stdin
+		if input_from_stdin:
+			input_file_temporary = True
+			input_file_obj = NamedTemporaryFile(delete=False)
+			input_file_obj.write(sys.stdin.read())
+			input_file_obj.close()
+			input_file = input_file_obj.name
 
-        # Create a temporary directory to extract our backup to
-        extract_root = mkdtemp()
-        backup_root = os.path.join(extract_root, 'backup')
-        database_root = os.path.join(backup_root, 'databases')
+		# Create a temporary directory to extract our backup to
+		extract_root = mkdtemp()
+		backup_root = os.path.join(extract_root, 'backup')
+		database_root = os.path.join(backup_root, 'databases')
 
-        # extract the gzipped tarball
-        with tarfile.open(input_file, 'r') as tf:
-            tf.extractall(extract_root)
+		# extract the gzipped tarball
+		with tarfile.open(input_file, 'r') as tf:
+			tf.extractall(extract_root)
 
-        # Restore databases
-        db_options = {}
-        if options.get('postgres_flags') is not None:
-            db_options['postgres_flags'] = options['postgres_flags']
-        db.restore_from(settings, database_root, **db_options)
+		# Restore databases
+		db_options = {}
+		if options.get('postgres_flags') is not None:
+			db_options['postgres_flags'] = options['postgres_flags']
+		db.restore_from(settings, database_root, **db_options)
 
-        # Restore media directory (if it has been backed up)
-        backup_media_root = os.path.join(backup_root, 'media')
-        if os.path.isdir(backup_media_root):
-            copy_tree(backup_media_root, media_root)
+		# Restore media directory (if it has been backed up)
+		backup_media_root = os.path.join(backup_root, 'media')
+		if os.path.isdir(backup_media_root):
+			copy_tree(backup_media_root, media_root)
 
-        # clean up
-        shutil.rmtree(extract_root, ignore_errors=True)
-        if input_file_temporary:
-            os.unlink(input_file)
+		# clean up
+		shutil.rmtree(extract_root, ignore_errors=True)
+		if input_file_temporary:
+			os.unlink(input_file)
